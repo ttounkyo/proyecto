89dd5f6a6978312115e7ff2e072424c9e9d3e82b
0wn3d <owernet513@gmail.com>
2016-01-07 18:11:22 +0100

    Alert desactivado

--- frontoffice/js/funciones_index.js
+++ frontoffice/js/funciones_index.js
@@ -1,6 +1,5 @@
 // Oculta o muestra un contenido 
 function showmenu(id) {
-	alert(id);	
   if(document.getElementById(id).style.display == "block") {
     document.getElementById(id).style.display = "none";
   }
