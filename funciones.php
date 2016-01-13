4ed217b0a899031cf3108afea6be4eb41b1632ba
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-01-13 16:24:30 +0100

    migas

--- funciones.php
+++ funciones.php
@@ -29,5 +29,23 @@ function desconectarBD($conexion){
     return substr_count($a ^ $b, "\0") * 2 === strlen($a . $b);
 }
 
-
+function breadcrumbs($separator = ' ? ', $home = 'Inicio') {
+    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+    $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+    $breadcrumbs = array('<a href="'. $base .'">'. $home .'</a>');
+ 
+    $last = end(array_keys($path));
+ 
+    foreach ($path as $x => $crumb) {
+        $title = ucwords(str_replace(array('.php', '_'), array('', ' '), $crumb));
+ 
+        if ($x != $last) {
+            $breadcrumbs[] = '<a href="'. $base . $crumb .'">'. $title .'</a>';
+        } else {
+            $breadcrumbs[] = $title;
+        }
+    }
+ 
+    return implode($separator, $breadcrumbs);
+}
  ?>
