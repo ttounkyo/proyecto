9b84d68533b10e52056859bf0df5d29cb7d424ba
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-01-13 16:07:37 +0100

    no message

--- backoffice/login.php
+++ backoffice/login.php
@@ -1,9 +1,6 @@
 <?php
-
-
-
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
 	session_start();
 	if(isset($_POST['nomuser']) && isset($_POST['pass'])){
 		$user = $_POST['nomuser'];
