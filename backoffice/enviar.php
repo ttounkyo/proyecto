2a79a2b031945fcacacf021e31e09c431c60ee4d
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-01-15 20:58:40 +0100

    mail

--- backoffice/enviar.php
+++ backoffice/enviar.php
@@ -18,27 +18,21 @@
 		$cliente		= "SELECT * FROM usuarios WHERE rol='cliente';";
 		$result_cli 	= $db->query($cliente) or die ($db->connect_error. " en la línea " . $db->connect_error);
 		
-		// while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)){
+		while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)){
 
-		// 	$nom 			= $registro['nombre'];
-		// 	$correo 		= $registro['email'];
-		// 	$titulo    		= 'Promoción!!!';
-		// 	$cabeceras 		= 'From: webmaster@example.com';
+			$nom 			= $registro['nombre'];
+			$correo 		= $registro['email'];
+			$titulo    		= 'Promoción!!!';
+			$cabeceras 		= 'From: webmaster@example.com';
 
-		// 	if(mail($correo, '$titulo', '$mensaje')){//, $cabeceras
-		// 		echo $correo;
-		// 		echo "<br>mensaje enviado!! a " . $nom ."<br>";
-		// 	}else{
-		// 		echo "error al enviar el mensaje.";
-		// 	}
-			
-		// }
-
-		if(mail('jguasch@iessacolomina.es', '$titulo', '$mensaje')){//, $cabeceras
-				echo "<br>mensaje enviado!! a Jose"  ."<br>";
+			if(mail($correo, '$titulo', '$mensaje')){//, $cabeceras
+				echo $correo;
+				echo "<br>mensaje enviado!! a " . $nom ."<br>";
 			}else{
 				echo "error al enviar el mensaje.";
 			}
+			sleep(10);
+		}
 			
 		desconectarBD($db);
 
