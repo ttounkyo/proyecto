35e2ee06f5e2f1b7e08000ece583d7fe81dfb1fe
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-01-15 20:05:19 +0100

    mail

--- backoffice/enviar.php
+++ backoffice/enviar.php
@@ -3,9 +3,11 @@
 
 	require_once('../funciones.php');
 
-	if(isset($_REQUEST['id']) && isset($_REQUEST['textmail'])){
+	if(isset($_REQUEST['id']) && isset($_POST['textmail'])){
+
 		$id = $_REQUEST['id'];
-		$mensaje = $_REQUEST['textmail'];
+		$mensaje = $_POST['textmail'];
+		
 		$db = conectarBD();
 		$verproductos 	= "SELECT titulo FROM productos WHERE idproducto = {$id}";
 		$resul_prod 	= $db->query($verproductos) or die ($db->connect_error. " en la línea " . $db->connect_error);
@@ -21,7 +23,7 @@
 			$nom 			= $registro['nombre'];
 			$correo 		= $registro['email'];
 			$para      		= $correo;
-			$titulo    		= 'Promoción!!!';s
+			$titulo    		= 'Promoción!!!';
 			$cabeceras 		= 'From: webmaster@example.com' . "\r\n" .
 			    'Reply-To: webmaster@example.com' . "\r\n" .
 			    'X-Mailer: PHP/' . phpversion();
@@ -34,10 +36,12 @@
 	}
 ?>
 
-<form action="index.php?sec=enviar&id<? echo $_REQUEST['id'] ?>" method="POST">
+<form action="index.php?sec=enviar&id<?php echo $_REQUEST['id']?>" method="POST">
 	<label for=""> Nombre Producto
-		<input type="text" disabled value="<?echo $_REQUEST['n']?>">
-	</label>
-	<textarea name="textmail" cols="30" rows="10">
-	</textarea>
+		<input type="text" disabled value="<?php echo $_REQUEST['n']?>">
+	</label><br>
+	<label for="">Mensaje promocional!</label><br>
+	<textarea name="textmail" cols="50" rows="5">
+	</textarea><br>
+	<button type="submit">Enviar</button>
 </form>
