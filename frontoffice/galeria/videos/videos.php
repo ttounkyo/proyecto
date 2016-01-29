e90c1c63b3c053cda5fa4e16de1f9b77d985e075
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-01-29 19:48:21 +0100

    galeria,fuente

--- frontoffice/galeria/videos/videos.php
+++ frontoffice/galeria/videos/videos.php
@@ -1,13 +1,15 @@
 <div class="vid">
-    <video width="425" height="350" controls>
+    <video controls>
         <source src="galeria/videos/oceans-clip.mp4" type="video/mp4" />
     </video>
 </div>
 <div class="vid">
-    <video width="425" height="350" controls>
+    <video controls>
         <source src="galeria/videos/competicion_converted.mp4" type="video/mp4" />
     </video>
 </div>
 <div class="vid">
+	<iframe  src="https://www.youtube.com/embed/L8Gs_eyIPFA" frameborder="0" allowfullscreen>
 
+	</iframe>
 </div>
