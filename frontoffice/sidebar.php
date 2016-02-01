7d09fb6f45ece468ff15d7f05e9a68b37710f22f
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-02-01 17:09:20 +0100

    gb

--- frontoffice/sidebar.php
+++ frontoffice/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="buscador">
-	
+
 <div class="busc">
 	<h1>BUSCADOR</h1>
 	<form id="buscador" name="buscador" method="post" action="index.php?sec=buscador">
@@ -9,29 +9,29 @@
 	<br>
 	<h1>CATEGORIAS</h1>
 </div >
-	
+
 	<?php
-		require_once("../funciones.php");
-		 
-		$db = conectarBD();
-	    $query = 'SELECT * FROM categorias;';
-	    $resultado = $db->query($query) or die ($db->connect_error. " en la línea ");
-	    desconectarBD($db);
-	?>
+require_once "../funciones.php";
+
+$db = conectarBD();
+$query = 'SELECT * FROM categorias;';
+$resultado = $db->query($query) or die($db->connect_error . " en la línea ");
+desconectarBD($db);
+?>
 
 	    <ul class="lista">
 			<?php
-				while($row = $resultado->fetch_array(MYSQLI_BOTH)){
-					echo "<li><a href=\"index.php?sec=buscador&id=".$row['nombre']."\">".$row['nombre']."</a></li>";
-				}
-			?>
+while ($row = $resultado->fetch_array(MYSQLI_BOTH)) {
+	echo "<li><a href=\"index.php?sec=buscador&id=" . $row['nombre'] . "\">" . $row['nombre'] . "</a></li>";
+}
+?>
 			<li><a href="index.php?sec=patines">home</a></li>
 	    </ul>
 
 	    <?php
-	if (isset($_SESSION["catg"])){
-		mysqli_free_result($result);
-		mysqli_close($db);
-	}
-	?>		
+if (isset($_SESSION["catg"])) {
+	mysqli_free_result($result);
+	mysqli_close($db);
+}
+?>
 </aside>
