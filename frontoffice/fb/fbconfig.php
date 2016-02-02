5f7d7b9b8f621a70534f9b6838cfa777cddc3de8
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-02-02 19:26:01 +0100

    fb

--- frontoffice/fb/fbconfig.php
+++ frontoffice/fb/fbconfig.php
@@ -2,9 +2,17 @@
 // session_start();
 // added in v4.0.0
 require_once 'autoload.php';
+use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
 use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
 use Facebook\FacebookSession;
 
 // init app with app id and secret
