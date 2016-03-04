bb270ec9b1018dca7a16da805b907e7d692c51e7
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-04 20:27:03 +0100

    mail

--- backoffice/mail/enviar.php
+++ backoffice/mail/enviar.php
@@ -1,54 +1,55 @@
 <h1>Enviamiento de correo</h1>
 <?php
 
-	require_once('../funciones.php');
+require_once '../../funciones.php';
 
-	if(isset($_REQUEST['id']) && isset($_POST['textmail'])){
+if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 
-		$id = $_REQUEST['id'];
-		$mensaje = $_POST['textmail'];
-		$emessage= "--".$uid."\n";
-	    $emessage.= "Content-type:text/html; charset=iso-8859-1\n";
-	    $emessage.= "Content-Transfer-Encoding: 7bit\n\n";
-	    $emessage .= $message."\n\n";
-	    $emessage.= "--".$uid."\n";
-	    $emessage .= "Content-Type: application/octet-stream; name=\"".$filename."\"\n"; // use different content types here
-	    $emessage .= "Content-Transfer-Encoding: base64\n";
-	    $emessage .= "Content-Disposition: attachment; filename=\"".$filename."\"\n\n";
-	    $emessage .= $content."\n\n";
-	    $emessage .= "--".$uid."--";
-		$db = conectarBD();
-		$verproductos 	= "SELECT marca FROM productos WHERE idproducto = {$id}";
-		$resul_prod 	= $db->query($verproductos) or die ($db->connect_error. " en la línea " . $db->connect_error);
-		// $rutaimg 	= mysqli_fetch_array($resul_prod)['ruta'];
-		$titulo 		= $resul_prod->fetch_array(MYSQLI_BOTH)['marca'];
+	$id = $_REQUEST['id'];
+	$mensaje = $_POST['textmail'];
+	$emessage = "--" . $uid . "\n";
+	$emessage .= "Content-type:text/html; charset=iso-8859-1\n";
+	$emessage .= "Content-Transfer-Encoding: 7bit\n\n";
+	$emessage .= $message . "\n\n";
+	$emessage .= "--" . $uid . "\n";
+	$emessage .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"\n"; // use different content types here
+	$emessage .= "Content-Transfer-Encoding: base64\n";
+	$emessage .= "Content-Disposition: attachment; filename=\"" . $filename . "\"\n\n";
+	$emessage .= $content . "\n\n";
+	$emessage .= "--" . $uid . "--";
+	$db = conectarBD();
+	$verproductos = "SELECT marca FROM productos WHERE idproducto = {$id}";
+	$resul_prod = $db->query($verproductos) or die($db->connect_error . " en la línea " . $db->connect_error);
+	// $rutaimg 	= mysqli_fetch_array($resul_prod)['ruta'];
+	$titulo = $resul_prod->fetch_array(MYSQLI_BOTH)['marca'];
 
-		// http://wp-a2z.com/oik_api/phpmailermsghtml/	
-		$cliente		= "SELECT * FROM usuarios WHERE rol='cliente';";
-		$result_cli 	= $db->query($cliente) or die ($db->connect_error. " en la línea " . $db->connect_error);
-		
-		while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)){
+	// http://wp-a2z.com/oik_api/phpmailermsghtml/
+	$cliente = "SELECT * FROM usuarios WHERE rol='cliente';";
+	$result_cli = $db->query($cliente) or die($db->connect_error . " en la línea " . $db->connect_error);
 
-			$nom 			= $registro['nombre'];
-			$correo 		= $registro['email'];
-			$cabeceras 		= 'From: webmaster@example.com';
+	while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)) {
 
-			if(mail($correo, $titulo, $mensaje, $cabeceras)){//,
-				echo "<br>mensaje enviado!! a " . $nom ."<br>";
-			}else{
-				echo "error al enviar el mensaje.";
-			}
-			sleep(3);
+		$nom = $registro['nombre'];
+		$correo = $registro['email'];
+		$cabeceras = 'From: webmaster@example.com';
+
+		if (mail($correo, $titulo, $mensaje, $cabeceras)) {
+//,
+			echo "<br>mensaje enviado!! a " . $nom . "<br>";
+		} else {
+			echo "error al enviar el mensaje.";
 		}
-			
-		desconectarBD($db);
-
+		sleep(3);
 	}
+
+	desconectarBD($db);
+
+}
 ?>
 
-<form action="index.php?sec=enviar&id=<?php echo $_REQUEST['id']?>" method="POST">
+<form action="index.php?sec=enviar&id=<?php echo $_REQUEST['id'] ?>" method="POST">
 	<label for=""> Nombre Producto
-		<!-- <input type="text" disabled value="<?php echo $_REQUEST['n']?>"> -->
+		<!-- <input type="text" disabled value="<?php echo $_REQUEST['n'] ?>"> -->
 	</label><br>
 	<label for="">Mensaje promocional!</label><br>
 	<textarea name="textmail" cols="50" rows="5"></textarea><br>
--- backoffice/mail/enviar2.php
+++ backoffice/mail/enviar2.php
@@ -1,101 +1,98 @@
 <h1>Enviamiento de correo</h1>
 <?php
 
-	require_once ('./PHPMailer-master/class.phpmailer.php');
-	require_once('../funciones.php');
-	require_once('./PHPMailerAutoload.php');
+require_once './PHPMailer-master/class.phpmailer.php';
+require_once '../../funciones.php';
+require_once './PHPMailerAutoload.php';
 
-	if(isset($_REQUEST['id'])){
-		$id = $_REQUEST['id'];
-		$db = conectarBD();
-		$verproductos 	= "SELECT titulo FROM productos WHERE idproducto = '$id";
-		$resul_prod 	= mysqli_query($db,$verproductos);
-		// $rutaimg 	= mysqli_fetch_array($resul_prod)['ruta'];
-		$titulo 		= mysqli_fetch_array($resul_prod)['titulo'];
-		
-		$cliente		= "SELECT * FROM usuarios WHERE rol='cliente';";
-		$result_cli 	= $db->query($cliente) or die ($db->connect_error. " en la línea ");
+if (isset($_REQUEST['id'])) {
+	$id = $_REQUEST['id'];
+	$db = conectarBD();
+	$verproductos = "SELECT titulo FROM productos WHERE idproducto = '$id";
+	$resul_prod = mysqli_query($db, $verproductos);
+	// $rutaimg 	= mysqli_fetch_array($resul_prod)['ruta'];
+	$titulo = mysqli_fetch_array($resul_prod)['titulo'];
 
-		$email 			  = new PHPMailer();
+	$cliente = "SELECT * FROM usuarios WHERE rol='cliente';";
+	$result_cli = $db->query($cliente) or die($db->connect_error . " en la línea ");
 
+	$email = new PHPMailer();
 
-		$email->From      = 'aa.antonio.delgado@gmail.com';
-		// $email-> poner el usuario y contraseña
-		$email->FromName  = 'Administrador';
-		$email->Subject   = "Promoción!!";
-		
-		while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)){
+	$email->From = 'aa.antonio.delgado@gmail.com';
+	// $email-> poner el usuario y contraseña
+	$email->FromName = 'Administrador';
+	$email->Subject = "Promoción!!";
 
-			$nom 			  = $regitro['nombre'];
-			$correo 		  = $regitro['email'];
-		
-			$mensaje 		  = "
+	while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)) {
+
+		$nom = $regitro['nombre'];
+		$correo = $regitro['email'];
+
+		$mensaje = "
 							<html>
 							<head>
 							  <title>Recordatorio de Producto en oferta</title>
 							</head>
 							<body>
 							  <h1>Producto en oferta.</h1>
-							  <h2>".$titulo."</h2>
-							  <p>Mensaje para ".$nom."</p>
+							  <h2>" . $titulo . "</h2>
+							  <p>Mensaje para " . $nom . "</p>
 							</body>
 							</html>
 							";
-			$email->Body      = $mensaje;
-			
-			$email->AddAddress($correo);
-			//$email->AddAddress( 'desti2' );
+		$email->Body = $mensaje;
 
-			// $file_to_attach = $_SERVER['DOCUMENT_ROOT'].$ruta;
+		$email->AddAddress($correo);
+		//$email->AddAddress( 'desti2' );
 
-			// $email->AddAttachment( $file_to_attach , 'logo.png' );
+		// $file_to_attach = $_SERVER['DOCUMENT_ROOT'].$ruta;
 
-			//$email->AddAttachment( $_SERVER['DOCUMENT_ROOT']."/img/cristalina.jpg" , 'cristalina' );
+		// $email->AddAttachment( $file_to_attach , 'logo.png' );
 
-			if($email->Send()){
-				echo "mensaje enviado al usuario ";
-			}else{
-				echo "no enviado";
-			}
-		}
-			
-		desconectarBD($db);
+		//$email->AddAttachment( $_SERVER['DOCUMENT_ROOT']."/img/cristalina.jpg" , 'cristalina' );
 
-
-
-		$mail = new PHPMailer;
-
-		//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'user@example.com';                 // SMTP username
-		$mail->Password = 'secret';                           // SMTP password
-		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		$mail->Port = 587;                                    // TCP port to connect to
-
-		$mail->setFrom('from@example.com', 'Mailer');
-		$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-		$mail->addAddress('ellen@example.com');               // Name is optional
-		$mail->addReplyTo('info@example.com', 'Information');
-		$mail->addCC('cc@example.com');
-		$mail->addBCC('bcc@example.com');
-
-		$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-		$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-		$mail->isHTML(true);                                  // Set email format to HTML
-
-		$mail->Subject = 'Here is the subject';
-		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-		if(!$mail->send()) {
-		    echo 'Message could not be sent.';
-		    echo 'Mailer Error: ' . $mail->ErrorInfo;
+		if ($email->Send()) {
+			echo "mensaje enviado al usuario ";
 		} else {
-		    echo 'Message has been sent';
+			echo "no enviado";
 		}
-
 	}
+
+	desconectarBD($db);
+
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+	$mail->isSMTP(); // Set mailer to use SMTP
+	$mail->Host = 'smtp1.example.com;smtp2.example.com'; // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true; // Enable SMTP authentication
+	$mail->Username = 'user@example.com'; // SMTP username
+	$mail->Password = 'secret'; // SMTP password
+	$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587; // TCP port to connect to
+
+	$mail->setFrom('from@example.com', 'Mailer');
+	$mail->addAddress('joe@example.net', 'Joe User'); // Add a recipient
+	$mail->addAddress('ellen@example.com'); // Name is optional
+	$mail->addReplyTo('info@example.com', 'Information');
+	$mail->addCC('cc@example.com');
+	$mail->addBCC('bcc@example.com');
+
+	$mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
+	$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); // Optional name
+	$mail->isHTML(true); // Set email format to HTML
+
+	$mail->Subject = 'Here is the subject';
+	$mail->Body = 'This is the HTML message body <b>in bold!</b>';
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	if (!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Message has been sent';
+	}
+
+}
 ?>
