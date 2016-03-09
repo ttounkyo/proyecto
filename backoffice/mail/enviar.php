968a105fc8097cd4dec38cbd183b3f72797b8656
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-09 01:57:51 +0100

    no message

--- backoffice/mail/enviar.php
+++ backoffice/mail/enviar.php
@@ -8,12 +8,14 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 
 	$uid = $_REQUEST['id'];
 	$mensaje = $_POST['textmail'];
+	$rango = $_POST['amountRange'];
 
 	$db = conectarBD();
-	$verproductos = "SELECT marca,ruta FROM productos WHERE idproducto ='$id'";
+	$verproductos = "SELECT marca,ruta,precio FROM productos WHERE idproducto ='$id'";
 	$resul_prod = $db->query($verproductos) or die($db->connect_error . " en la línea " . $db->connect_errno);
 	$marca = $resul_prod->fetch_array(MYSQLI_BOTH)['marca'];
 	$imagen = $resul_prod->fetch_array(MYSQLI_BOTH)['ruta'];
+	$precio = $resul_prod->fetch_array(MYSQLI_BOTH)['precio'];
 
 	$cliente = "SELECT * FROM usuarios WHERE rol='cliente';";
 	$result_cli = $db->query($cliente) or die($db->connect_error . " en la línea " . $db->connect_errno);
@@ -45,7 +47,7 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 			 * $correo->IsHTML(false);
 			 * $correo->Body = "Mi mensaje en Texto Plano";
 		*/
-		$correo->MsgHTML("<h1>" . $nomP . "</h1><p>" . $descr . "</p><br>" . $text . "<br><h2>Preu: " . $preu . "</h2><b><em>Descompte: " . $descompte . "</em></b>");
+		$correo->MsgHTML("<h1>" . $nom . "</h1><p>" . $marca . "</p><br>" . $mensaje . "<br><h2>Preu: " . $precio . "€</h2><b><em>Descompte: " . $rango . "</em></b>");
 
 		//Si deseamos agregar un archivo adjunto utilizamos AddAttachment
 		$correo->AddAttachment($imagen);
@@ -70,5 +72,7 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 	</label><br>
 	<label for="">Mensaje promocional!</label><br>
 	<textarea name="textmail" cols="50" rows="5"></textarea><br>
+	<input type="range" name="amountRange" min="5" max="90" value="5" oninput="this.form.amountInput.value=this.value"/>
+    <input type="number" name="amountInput" min="5" max="90" value="5" oninput="this.form.amountRange.value=this.value" />%<br>
 	<button type="submit">Enviar</button>
 </form>
