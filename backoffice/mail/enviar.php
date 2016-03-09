6aae679db339ac9c846ab246261442c44fa42836
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-09 02:41:14 +0100

    mail

--- backoffice/mail/enviar.php
+++ backoffice/mail/enviar.php
@@ -23,25 +23,32 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 	$cliente = "SELECT * FROM usuarios WHERE rol='cliente';";
 	$result_cli = $db->query($cliente) or die($db->connect_error . " en la línea " . $db->connect_errno);
 
+	$correo = new PHPMailer(); //Creamos una instancia en lugar usar mail()
+	$correo->IsSMTP();
+	$correo->SMTPAuth = true;
+	$correo->SMTPSecure = 'tls';
+	$correo->Host = "smtp.gmail.com";
+	$correo->Port = 587;
+	$correo->Username = "ttounkyo@gmail.com";
+	$correo->Password = "ttounkyodaw2016";
+
 	while ($registro = $result_cli->fetch_array(MYSQLI_BOTH)) {
 
 		$nom = $registro['nombre'];
 		$correocliente = $registro['email'];
 		$cabeceras = 'webmaster@example.com';
 
-		$correo = new PHPMailer(); //Creamos una instancia en lugar usar mail()
-
 		//Usamos el SetFrom para decirle al script quien envia el correo
-		$correo->SetFrom($cabeceras, "Mi Codigo PHP");
+		$correo->SetFrom("ttounkyo@gmail.com", "Cuenta de administrador");
 
 		//Usamos el AddReplyTo para decirle al script a quien tiene que responder el correo
-		$correo->AddReplyTo("me@micodigophp.com", "Mi Codigo PHP");
+		$correo->AddReplyTo("ttounkyo@gmail.com", "Cuenta de administrador");
 
 		//Usamos el AddAddress para agregar un destinatario
 		$correo->AddAddress($correocliente);
 
 		//Ponemos el asunto del mensaje
-		$correo->Subject = "Oferta";
+		$correo->Subject = "Oferta!!";
 
 		/*
 			 * Si deseamos enviar un correo con formato HTML utilizaremos MsgHTML:
@@ -51,9 +58,9 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 			 * $correo->Body = "Mi mensaje en Texto Plano";
 		*/
 		$correo->MsgHTML("
-<img src='../../frontoffice/imagenes/logo.png' alt='logo'>
-<h1> " . $nom . "</h1><p>" . $marca . "</p><br>" . $mensaje . "<br><h2>Preu:  " . $precio . " €</h2><b><em>Descompte: " . $rango . "%</em></b>
-");
+			<img src='../../frontoffice/imagenes/logo.png' alt='logo'>
+			<h1> " . $nom . "</h1><h2>Marca: <i>" . $marca . "</i></h2><br>" . $mensaje . "<br><h2>Preu:  " . $precio . " €</h2><b><em>Descompte: " . $rango . "%</em></b>
+			");
 
 		//Si deseamos agregar un archivo adjunto utilizamos AddAttachment
 		$correo->AddAttachment($imagen);
@@ -63,6 +70,7 @@ if (isset($_REQUEST['id']) && isset($_POST['textmail'])) {
 			echo "Hubo un error: " . $correo->ErrorInfo . "<br>";
 		} else {
 			echo "Mensaje enviado con exito a " . $nom . "<br>";
+			header("location: ../principal/index.php");
 		}
 		sleep(3);
 	}
