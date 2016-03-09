54d7accd9b0b06cd2cf3602b9c07c256c592da4c
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-09 07:15:24 +0100

    no message

--- frontoffice/factura/compra.php
+++ frontoffice/factura/compra.php
@@ -48,7 +48,7 @@ if (!empty($_SESSION['usuariofront']) || !empty($_SESSION['usuario'])) {
 // (c) Xavier Nicolay
 // Exemple de génération de devis/facture PDF
 //ob_clean();
-require '../factura/invoice.php';
+require 'invoice.php';
 //ob_get_clean();
 $pdf = new PDF_Invoice('P', 'mm', 'A4');
 $pdf->AddPage();
