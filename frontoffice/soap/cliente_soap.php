15187915414d75c3d1376da0e29dae382a6c43d4
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-11 20:56:53 +0100

    no message

--- frontoffice/soap/cliente_soap.php
+++ frontoffice/soap/cliente_soap.php
@@ -26,7 +26,7 @@ require_once "../vendor/autoload.php";
 // 	}
 // }
 // $cliente2 = new nusoap_client("http://localhost/DAW/DEWS/hola/frontoffice/producto_controlador.php?wsdl", true);
-$cliente2 = new nusoap_client("http://http://ttounkyo-ttounkyo.rhcloud.com/frontoffice/soap/producto_controlador.php?wsdl", true);
+$cliente2 = new nusoap_client("http://ttounkyo-ttounkyo.rhcloud.com/frontoffice/soap/producto_controlador.php?wsdl", true);
 
 $error = $cliente2->getError();
 
