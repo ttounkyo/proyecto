cba2cfeb41e72d4ef148f5e74cfc244b119b05f1
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-17 16:28:43 +0100

    no message

--- frontoffice/factura/compra.php
+++ frontoffice/factura/compra.php
@@ -156,7 +156,8 @@ try {
 	// $content = ob_get_clean();
 	$html2pdf->writeHTML($content);
 
-	// $html2pdf->Output("./control/carro" . $registro . ".pdf", "F");
+	$html2pdf->Output("../factura/control/carro" . $registro . ".pdf", "F");
+	$correo->AddAttachment("../factura/control/carro" . $registro . ".pdf", "CARRO");
 	ob_get_clean();
 	// $html2pdf->Output();
 } catch (Html2PdfException $e) {
@@ -172,7 +173,6 @@ $pdf->Output("../factura/control/factura" . $registro . ".pdf", "F");
 
 //Si deseamos agregar un archivo adjunto utilizamos AddAttachment
 $correo->AddAttachment("../factura/control/factura" . $registro . ".pdf", "FACTURA");
-// $correo->AddAttachment("../factura/control/carro" . $registro . ".pdf", "CARRO");
 
 //Enviamos el correo
 if (!$correo->Send()) {
