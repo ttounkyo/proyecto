d0356d7f19c2f1e6778a4438e85eec493704b65f
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-17 07:25:52 +0100

    no message

--- frontoffice/carro_pdf.php
+++ frontoffice/carro_pdf.php
@@ -49,7 +49,7 @@ height: 103px;
         <table class="page_header" >
             <tr>
                 <td style="width: 100%; text-align: left">
-                    <img src="./imagenes/logo.png" alt="logo">
+                    <img src="../imagenes/logo.png" alt="logo">
                 </td>
             </tr>
         </table>
