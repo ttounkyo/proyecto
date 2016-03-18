704d43a2e51ea3c0887bcac89b34264965fb0716
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-18 07:44:35 +0100

    no message

--- frontoffice/patines.php
+++ frontoffice/patines.php
@@ -1,33 +1,33 @@
-<?php 
-	require_once("../funciones.php");
- 
-	$db = conectarBD();
-		if($db->connect_errno > 0){
-		    die('Imposible conectar [' . $db->connect_error . ']');
-		}
+<?php
+require_once "../funciones.php";
 
-	$verproductos = 'SELECT * FROM productos JOIN categorias_productos USING (idproducto)
+$db = conectarBD();
+if ($db->connect_errno > 0) {
+	die('Imposible conectar [' . $db->connect_error . ']');
+}
+
+$verproductos = 'SELECT * FROM productos JOIN categorias_productos USING (idproducto)
 					JOIN categorias USING(idcategoria) GROUP BY idproducto';
-	$result_vpro = $db->query($verproductos) or die ($db->connect_error. " en la línea ");
-	$products='';
-	while ($registro = $result_vpro->fetch_array(MYSQLI_BOTH)){
+$result_vpro = $db->query($verproductos) or die($db->connect_error . " en la línea ");
+$products = '';
+while ($registro = $result_vpro->fetch_array(MYSQLI_BOTH)) {
 
-		$products .='
+	$products .= '
 					<div class="product">
-						<div class="title">'.$registro['titulo'].'</div>
-						<div class="pic"><img src="../backoffice/'.str_replace("../", "", $registro['ruta']).'" width="128" height="128" alt="'.htmlspecialchars($registro['titulo']).'" /></div>
-						<div class="description">'.$registro['descripcion'].'</div>
-						<div class="price">'.$registro['precio'].'€</div><br>
-						<!--<div class="cantidad">'.$registro['cantidad'].'</div>-->
-						<div class="buton"><button class="btn"><a href="index.php?sec=pedido&id='.$registro["idproducto"].'">Añadir</a></button></div>
+						<div class="title">' . $registro['titulo'] . '</div>
+						<div class="pic"><img src="../backoffice/' . str_replace("../", "", $registro['ruta']) . '" width="128" height="128" alt="' . htmlspecialchars($registro['titulo']) . '" /></div>
+						<div class="description">' . $registro['descripcion'] . '</div>
+						<div class="price">' . $registro['precio'] . '€</div><br>
+						<!--<div class="cantidad">' . $registro['cantidad'] . '</div>-->
+						<div class="buton"><a href="index.php?sec=pedido&id=' . $registro["idproducto"] . '"><button class="btn">Añadir</button></a></div>
 					</div>
 					';
-		// Class pedir quedarme con el identificador de producto
-	}
+	// Class pedir quedarme con el identificador de producto
+}
 
-	desconectarBD($db);
+desconectarBD($db);
 
 ?>
-		<div id="lista">	
-		<?=$products?>	
+		<div id="lista">
+		<?=$products?>
 		</div>
