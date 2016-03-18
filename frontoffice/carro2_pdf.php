f2271c5840480ce9d5b76c43d3c7cc60f6a0232c
Antonio Delgado Moreira <aa.antonio.delgado@gmail.com>
2016-03-18 09:15:47 +0100

    no message

---
+++ frontoffice/carro2_pdf.php
@@ -0,0 +1,91 @@
+<style type="text/css">
+<!--
+table#listadopro {
+/* font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;*/
+font-size: 15px;
+margin: 45px;
+/* width: 238px; */
+/* text-align: left; */
+border-collapse: collapse;
+}
+table.page_header {
+width: 100%;
+min-height: 150px;
+background-color: #483E3D;
+padding: 2mm
+}
+span{
+text-align: center;
+}
+p{
+text-align: center;
+}
+th {
+font-size: 16px;
+font-weight: normal;
+padding: 3px;
+background-color: white;
+border: 2px solid #e3e3e3;
+/* border-bottom: 1px solid #fff; */
+color: black;
+}
+tr.algo{
+border: 2px solid #E3E3E3;
+}
+td {
+padding: 14px;
+background-color: white;
+color: #52625A;
+font-size: 10px;
+border: 2px solid #e3e3e3;
+}
+img#imagen {
+width: 109px;
+height: 103px;
+}
+-->
+</style>
+<page backtop="7mm" backbottom="7mm" backleft="10mm" backright="10mm"><page_header>
+        <table class="page_header" >
+            <tr>
+                <td style="width: 100%; text-align: left">
+                    <img src="./imagenes/logo.png" alt="logo">
+                </td>
+            </tr>
+        </table>
+    </page_header>
+    <page_footer>
+        <p>
+            © 1996-2015, ttounkyo.com, Inc. o afiliados. Todos los derechos reservados.<br>
+            ¿Necesitas ayuda?<br>
+            Localizar o gestionar compras<br>
+            Tarifas y políticas de envío<br>
+            Devolver o reemplazar productos<br>
+        </p>
+    </page_footer><br><br><br><br><br><br>
+    <table id="listadopro">
+        <tr class="algo">
+            <th>Imagen</th>
+            <th>Titulo</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
+            <th>Marca</th>
+            <th>Cantidad</th>
+        </tr>
+        <?php
+if (isset($_SESSION['carrito'])) {
+	foreach ($_SESSION['carrito'] as $key => $value) {
+		?>
+        <tr class="algo">
+            <td><img id="imagen" src="../../backoffice/<?=$value['ruta']?>" alt="imagen" /></td>
+            <td><?=$value['titulo']?></td>
+            <td><?=$value['descripcion']?></td>
+            <td><?=$value['precio']?> €</td>
+            <td><?=$value['marca']?></td>
+            <td><?=$value['cantidad']?></td>
+        </tr>
+        <?php
+}
+}
+?>
+    </table></page>
